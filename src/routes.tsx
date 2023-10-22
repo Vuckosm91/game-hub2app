a459c72ad78755ed78d20abc81518dbe724c8362
Vucko <vuckosm91@gmail.com>
2023-10-22 16:13:49 +0200

    moved the entities

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import ErrorPage from "./pages/ErrorPage";
 import GameDetailPage from "./pages/GameDetailPage";
-import HomePAge from "./pages/HomePAge";
+import HomePage from "./pages/HomePage";
 import Layout from "./pages/Layout";
 
 const router = createBrowserRouter([
@@ -10,7 +10,7 @@ const router = createBrowserRouter([
     element: <Layout />,
     errorElement: <ErrorPage />,
     children: [
-      { index: true, element: <HomePAge /> },
+      { index: true, element: <HomePage /> },
       { path: "/games/:slug", element: <GameDetailPage /> },
     ],
   },
